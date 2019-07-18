0c2be988d40c166ef35c53812b20d1aa12e2bc29
Itstep <seleznev_k@itstep.org>
2019-07-18 19:40:44 +0300

    Установлен Spring Boot framework

---
+++ src/main/java/org/itstep/demoshop318/Demoshop318Application.java
@@ -0,0 +1,13 @@
+package org.itstep.demoshop318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoshop318Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoshop318Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/itstep/demoshop318/Demoshop318ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.itstep.demoshop318;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demoshop318ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
